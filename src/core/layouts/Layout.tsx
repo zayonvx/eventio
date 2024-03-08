060448ba51e96d6df4680ebad01e0bd3c8966733
zayonvx <zayonvx@gmail.com>
2024-03-08 19:26:02 +0600

    appshell added

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import React, { Suspense } from "react"
 import { BlitzLayout } from "@blitzjs/next"
+import { AppShell, Footer, Header, Navbar, Text } from "@mantine/core"
+import { Horizontal, Vertical } from "mantine-layout-components"
 
 type Props = {
   title?: string
@@ -8,22 +10,42 @@ type Props = {
   maxWidth?: number
 }
 const Layout: BlitzLayout<Props> = ({ title, children, maxWidth = 800 }) => {
+  const thisYear = new Date().getFullYear()
   return (
     <>
       <Head>
         <title>{title || "eventio"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Suspense fallback="Loading...">
-        <div
-          style={{
-            width: "100%",
-            maxWidth: maxWidth,
-          }}
-        >
-          {children}
-        </div>
-      </Suspense>
+      <AppShell
+        padding="md"
+        header={
+          <Header height={45} p="xs">
+            <Horizontal fullH>
+              <Text fw="bold">Eventio</Text>
+            </Horizontal>
+          </Header>
+        }
+        footer={
+          <Footer height={35}>
+            <Horizontal fullW fullH center>
+              <Text fz="xs" color="dimmed">
+                copyright {thisYear}
+              </Text>
+            </Horizontal>
+          </Footer>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Vertical fullW fullH>
+          <Suspense fallback="Loading...">{children}</Suspense>
+        </Vertical>
+      </AppShell>
     </>
   )
 }
